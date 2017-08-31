02e083d4b85236f49db2eed129c34571eca788f3
Pathanin Lokbow <plokbow@gmail.com>
2017-08-31 14:42:13 +0700

    หาพื้นไม่ต้องหาสีดำแล้ว ถ้าชนก็จไปเริ่มใหม่ด้วย

--- _main.cpp
+++ _main.cpp
@@ -18,10 +18,11 @@ using namespace std;
 bool isRecord = true;
 int frontLeft, frontRight, lefts, rights;
 
-CreateData	robotData;
-RobotConnector	robot;
+CreateData robotData;
+RobotConnector robot;
 
-void updateData() {
+void updateData()
+{
 	cvWaitKey(20);
 
 	if (!robot.ReadData(robotData))
@@ -31,32 +32,35 @@ void updateData() {
 	frontRight = robotData.cliffSignal[2];
 	lefts = robotData.cliffSignal[0];
 	rights = robotData.cliffSignal[3];
-
 }
-void walk(double vl, double vr) {
-	if (!robot.DriveDirect((int)(vl*Create_MaxVel), (int)(vr*Create_MaxVel)))
+void walk(double vl, double vr)
+{
+	if (!robot.DriveDirect((int)(vl * Create_MaxVel), (int)(vr * Create_MaxVel)))
 		cout << "SetControl Fail" << endl;
 }
 
-int findDirection() {
+int findDirection()
+{
 
 	updateData();
-	
+
 	// black and white
 	return (frontLeft < cut && frontRight > cut);
 }
 
-void printSensor() {
+void printSensor()
+{
 	updateData();
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++)
+	{
 		cout << robotData.cliffSignal[i] << "\t";
 	}
 	cout << endl;
-
 }
 
-int getCut(int  type) {
+int getCut(int type)
+{
 	if (type == 0)
 		return 990;
 	else if (type == 1)
@@ -67,12 +71,62 @@ int getCut(int  type) {
 		return 845;
 }
 
-bool isWhite(int val, int type) {
-	return (val > getCut(type) );
+bool isWhite(int val, int type)
+{
+	return (val > getCut(type));
 }
 
-bool isBlack(int val, int type) {
-	return (val < getCut(type) );
+bool isBlack(int val, int type)
+{
+	return (val < getCut(type));
+}
+
+bool findPath()
+{
+	while (!(robotData.bumper[0] || robotData.bumper[1]))
+	{
+		updateData();
+		walk(1, 1);
+	}
+
+	// walk back
+	for (int i = 0; i < 5000; i++)
+	{
+		walk(-1, -1);
+	}
+
+	// spin 5 sec
+	for (int i = 0; i < 5000; i++)
+	{
+		walk(1, -1);
+	}
+	walk(0, 0);
+
+	/* while (!(isBlack(frontLeft, 1) && isBlack(frontRight, 2) && isBlack(lefts, 0) && isBlack(rights, 3)))
+	{
+		walk(1, 1);
+	} */
+	while (!(isWhite(frontLeft, 1) && isWhite(frontRight, 2) && isWhite(lefts, 0) && isWhite(rights, 3)))
+	{
+		walk(1, 1);
+		if ((robotData.bumper[0] || robotData.bumper[1]))
+			return false;
+	}
+	/* while (!(isBlack(frontLeft, 1) && isBlack(frontRight, 2) && isBlack(lefts, 0) && isBlack(rights, 3)))
+	{
+		walk(1, 1);
+	} */
+	while (!(isWhite(frontLeft, 1) && isWhite(frontRight, 2)))
+	{
+		walk(1, 1);
+		if ((robotData.bumper[0] || robotData.bumper[1]))
+			return false;
+	}
+	while (isWhite(frontLeft, 1) != isWhite(frontRight, 2))
+	{
+		walk(1, -1);
+	}
+	return true;
 }
 
 int main()
@@ -90,133 +144,112 @@ int main()
 	robot.DriveDirect(0, 0);
 	cvNamedWindow("Robot");
 
-
 	// wait key to start track line
-	char c = cvWaitKey(0);	
+	char c = cvWaitKey(0);
 
 	// initialize sensor data
 	updateData();
 
-	if (c == 'a') {
-		while (!(robotData.bumper[0] || robotData.bumper[1])) {
-			updateData();
-			walk(1, 1);
+	// bump and find the path
+	if (c == 'a')
+	{
+		bool onPath = false;
+		onPath = findPath();
+		while (!onPath)
+		{
+			onPath = findPath();
 		}
-
-		for (int i = 0; i < 10000; i++) {
-			
-			walk(-1, -1);
-		}
-		
-		for (int i = 0; i < 10000; i++) {
-			walk(1, -1);
-		}
-		walk(0, 0);
-
-		while (!(isBlack(frontLeft, 1) && isBlack(frontRight, 2) && isBlack(lefts, 0) && isBlack(rights, 3))) {
-			walk(1, 1);
-		}
-		while (!(isWhite(frontLeft, 1) && isWhite(frontRight, 2) && isWhite(lefts, 0) && isWhite(rights, 3))) {
-			walk(1, 1);
-		}
-		while (!(isBlack(frontLeft, 1) && isBlack(frontRight, 2) && isBlack(lefts, 0) && isBlack(rights, 3))) {
-			walk(1, 1);
-		}
-		while (!(isWhite(frontLeft, 1) && isWhite(frontRight, 2))) {
-			walk(1, 1);
-		}
-		while (isWhite(frontLeft, 1) != isWhite(frontRight, 2)) {
-			walk(1, -1);
-		}
-		
 	}
-	
-
-
 
 	// find direction of robot
 	bool clockwise = findDirection();
-
 
 	// default is counter clockwise
 	while (true)
 	{
 		char btn = cvWaitKey(20);
-		if (btn == 'b') break;
+		if (btn == 'b')
+			break;
 
 		double vl = 0, vr = 0;
 
 		//////////////////////////////////////////////
 		// change direction of robot
 		//////////////////////////////////////////////
-		if (btn == 't') {
-			while (true) {
+		if (btn == 't')
+		{
+			while (true)
+			{
 				updateData();
 				vl = -1;
 				vr = 1;
 
 				walk(vl, vr);
 
-				if (clockwise && isWhite(frontLeft,1) && isBlack(frontRight,2))
+				if (clockwise && isWhite(frontLeft, 1) && isBlack(frontRight, 2))
 					break;
 
-				if (!clockwise && isBlack(frontLeft,1) && isWhite(frontRight,2))
+				if (!clockwise && isBlack(frontLeft, 1) && isWhite(frontRight, 2))
 					break;
-
-
 			}
 			clockwise = !clockwise;
 		}
 		//////////////////////////////////////////////
-
-
 
 		//////////////////////////////////////////////
 		// control (left, right and up
 		//////////////////////////////////////////////
 		// BOTH BLACK
 		cout << lefts << " " << frontLeft << " " << frontRight << " " << rights << endl;
-		if (isBlack(frontLeft, 1) && isBlack(frontRight, 2) && isBlack(lefts, 0) && isBlack(rights,3)) {
+		if (isBlack(frontLeft, 1) && isBlack(frontRight, 2) && isBlack(lefts, 0) && isBlack(rights, 3))
+		{
 			// cout << "all black" b<< endl;
-			while (true) {
+			while (true)
+			{
 				updateData();
 
-				if (clockwise) {
+				if (clockwise)
+				{
 					vl = 1;
 					vr = 0.2;
 				}
-				else {
+				else
+				{
 					vl = 0.2;
 					vr = 1;
 				}
 
 				walk(vl, vr);
 
-				if ((isWhite(frontLeft,1) && isWhite(frontRight,2)) || (isWhite(frontLeft, 1) != isWhite(frontRight, 2))) {
+				if ((isWhite(frontLeft, 1) && isWhite(frontRight, 2)) || (isWhite(frontLeft, 1) != isWhite(frontRight, 2)))
+				{
 					break;
 				}
 			}
 		}
-		else if (isBlack(frontLeft,1) && isBlack(frontRight,2)) {
+		else if (isBlack(frontLeft, 1) && isBlack(frontRight, 2))
+		{
 			vl = 1;
 			vr = 0.65;
 		}
 		// BOTH WHITE
-		else if (isWhite(frontLeft,1) && isWhite(frontRight,2)) {
+		else if (isWhite(frontLeft, 1) && isWhite(frontRight, 2))
+		{
 			vl = 0.65;
 			vr = 1;
 		}
 		// WHITE AND BLACK
-		else if (isWhite(frontLeft,1) != isWhite(frontRight,2)) {
+		else if (isWhite(frontLeft, 1) != isWhite(frontRight, 2))
+		{
 			vl = 2;
 			vr = 2;
 		}
 
-
 		//////////////////////////////////////////////
 		// swap velocity of each wheel if direction of robot is clockwise
 		//////////////////////////////////////////////
-		if (!clockwise) {
+		if (!clockwise)
+		{
 			double temp = vl;
 			vl = vr;
 			vr = temp;
@@ -227,16 +260,13 @@ int main()
 		//////////////////////////////////////////////
 		walk(vl, vr);
 
-
 		updateData();
 		//////////////////////////////////////////////
 		// show value of cliff sensor
 		//////////////////////////////////////////////
 		//printSensor();
-
-
 	}
-		cvWaitKey(0);
+	cvWaitKey(0);
 
 	robot.Disconnect();
 
